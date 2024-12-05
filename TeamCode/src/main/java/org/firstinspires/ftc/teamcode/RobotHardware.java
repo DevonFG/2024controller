31234dac074cb41e6970dacff0c77dc36b2d2cf2
Devon <devon6608@gmail.com>
2024-12-05 15:20:49 -0800

    Update RobotHardware.java
    Made the wrappers for all instructions we currently know we need.
    Need to make/fix the inside of the wrappers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -116,7 +116,7 @@ public class RobotHardware {
      * @param Drive     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
      * @param Turn      Right/Left turning power (-1.0 to 1.0) +ve is CW
      */
-    public void driveRobot(double Drive, double Turn) {
+    public void driveRobot(double Drive, double Turn, double Strafe) {
         // Combine drive and turn for blended motion.
         double left  = Drive + Turn;
         double right = Drive - Turn;
@@ -138,30 +138,45 @@ public class RobotHardware {
      *
      * @param leftWheel     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
      * @param rightWheel    Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
-     */
-    public void setDrivePower(double leftWheel, double rightWheel) {
-        // Output the values to the motor drives.
-        leftDrive.setPower(leftWheel);
-        rightDrive.setPower(rightWheel);
-    }
+    //  */
+    // public void setDrivePower(double leftWheel, double rightWheel) {
+    //     // Output the values to the motor drives.
+    //     leftDrive.setPower(leftWheel);
+    //     rightDrive.setPower(rightWheel);
+    // }
 
     /**
      * Pass the requested arm power to the appropriate hardware drive motor
      *
      * @param power driving power (-1.0 to 1.0)
-     */
-    public void setArmPower(double power) {
-        armMotor.setPower(power);
-    }
+    //  */
+    // public void setArmPower(double power) {
+    //     armMotor.setPower(power);
+    // }
 
     /**
      * Send the two hand-servos to opposing (mirrored) positions, based on the passed offset.
      *
      * @param offset
      */
-    public void setHandPositions(double offset) {
+    public void setSweeperPosition(double offset) {
         offset = Range.clip(offset, -0.5, 0.5);
         leftHand.setPosition(MID_SERVO + offset);
         rightHand.setPosition(MID_SERVO - offset);
+    }
+
+    public void standUp(double inches) {
+    }
+
+    public void liftScrew(double inches) {
+    }
+
+    public void toggleSweeper() {
+    }
+
+    public void setScrewPower(double spin) {
+    }
+
+    public void toggleDepositDoor() {
     }
 }
